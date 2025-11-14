c3d419f664f41d2a5592118aebbef326e2316a4c
Jacob McMurtry <mcjacob@ifarm2402.jlab.org>
2025-11-14 16:32:44 -0500

    Added FPP event selection cuts

--- GetAsymmetry.C
+++ GetAsymmetry.C
@@ -1,8 +1,11 @@
-// ******************************************************************************** //
-//                    Created by J.McMurtry on August 12th, 2025                    //
-//                                                                                  //
-//              Script to extract azimuthal distribution and asymmetry              //
-// ******************************************************************************** //
+// ------------------------------------------------------------------------ //
+// Script to extract azimuthal distribution and helicity asymmetry for GEp  //
+//                                                                          //
+// ---------                                                                //
+//  Jacob McMurtry, rby2vw@virginia.edu CREATED 08-12-2025                  //
+// ---------                                                                //
+// ** Do not tamper with this sticker! Log any updates to the script above. //
+// ------------------------------------------------------------------------ //
 
 #include "TChain.h"
 #include "TTree.h"
@@ -13,6 +16,7 @@
 #include "TF1.h"
 #include "TTreeFormula.h"
 #include <map>
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include "TString.h"
@@ -148,11 +152,42 @@ struct RunData{
   double charge;
 };
 
+// Function to draw asymmetry with lines at each bin
+void DrawAsymmetry(TH1* h, double frac = 0.4, int color = kRed, int linewidth = 2, bool drawAxis = true, bool forceYminZero = false) {
+    if (!h) return;
+
+    // Force drawing into the current pad
+    if (gPad) gPad->cd();
+
+    h->SetLineColor(color);
+    h->SetLineWidth(linewidth);
+    h->SetMarkerStyle(20);
+    h->SetMarkerColor(color);
+
+    if (drawAxis) h->Draw("P"); // only draw axes the first time
+    if (forceYminZero) h->SetMinimum(0); //Set axis min to zero if desired
+
+    int nbins = h->GetNbinsX();
+    for (int i = 1; i <= nbins; i++) {
+        double x = h->GetBinCenter(i);
+        double y = h->GetBinContent(i);
+        double binWidth = h->GetBinWidth(i);
+        double half = 0.5 * binWidth * frac;
+        TLine *line = new TLine(x - half, y, x + half, y);
+        line->SetLineColor(color);
+        line->SetLineWidth(linewidth);
+        line->Draw("same");
+    }
+
+    h->Draw("P same");
+}
+
 
 // **** ========== Main functions ========== **** 
-void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge.txt", const char *outfilename="output/Asymmetry.root"){
+void GetAsymmetry( const char *configfilename="configElastic.cfg", const char *chargefile="runCharge.txt", const char *outfilename="output/Asymmetry.root"){
 
   gErrorIgnoreLevel = kError; // Ignores all ROOT warnings
+  gStyle->SetOptStat(0); // No stats box on histograms
 
   // Define a clock to check macro processing time
   TStopwatch *sw = new TStopwatch();
@@ -187,7 +222,21 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
       globalcut += currentline.Data();
     }
   }
-  
+
+  TCut selectioncut = "";
+  while( currentline.ReadLine(infile) && !currentline.BeginsWith("endselection") ){
+    if( !currentline.BeginsWith("#") ){
+      selectioncut += currentline.Data();
+    }
+  }
+
+  //while ( currentline.ReadLine(infile) && !currentline.BeginsWith("endcut") ) {
+  //  currentline = currentline.Strip(TString::kBoth); // remove whitespace
+  //  if (currentline.Length() == 0 || currentline.BeginsWith("#")) continue; // skip blank or commented lines
+
+
+  cout << "Global cut = " << globalcut.GetTitle() << endl;
+  cout << "Selection cut = " << selectioncut.GetTitle() << endl;
   cout << "Let's ball" << endl;
   
   // Time to initialize branches we will use:
@@ -199,7 +248,8 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
   double runnum[1];
   double dpp[MAXHEEP];
   double sclose[MAXHEEP], zclose[MAXHEEP];
-  double ttheta[MAXHEEP];
+  double ttheta[MAXHEEP], tphi[MAXHEEP];
+  double true_hel[1];
 
   // Why are the branches disabled here? To make it run FASTER by only activating the ones you need!
   // the * applies it to all branches, the 0 disables those branches. to enable would need to make 1
@@ -207,11 +257,15 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
 
   // Initializing branches
   C->SetBranchStatus("heep.*",1);
+  C->SetBranchStatus("scalhel.true_hel",1);
   C->SetBranchStatus("sbs.gemFT.track.*",1);
   C->SetBranchStatus("g.runnum",1);
   C->SetBranchStatus("earm.ecal.*",1);
   C->SetBranchStatus("sbs.gemFPP.track.*",1);
   C->SetBranchStatus("sbs.tr.*",1);
+  C->SetBranchStatus("sbs.hcal.nblk*",1);
+  C->SetBranchStatus("sbs.hcal.y*",1);
+  C->SetBranchStatus("sbs.hcal.x*",1);
 
   // Filling Arrays
   C->SetBranchAddress("heep.dxECAL", dxECAL);
@@ -223,6 +277,8 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
   C->SetBranchAddress("sbs.gemFPP.track.sclose", sclose);
   C->SetBranchAddress("sbs.gemFPP.track.zclose", zclose);
   C->SetBranchAddress("sbs.gemFPP.track.theta", ttheta);
+  C->SetBranchAddress("sbs.gemFPP.track.phi", tphi);
+  C->SetBranchAddress("scalhel.true_hel", true_hel);
   
 
   TFile *fout = new TFile(outfilename, "RECREATE");
@@ -238,18 +294,25 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
   TH1D *hEdivP = new TH1D("hEdivP", "E/P after global cut; E/P;", bins, 0.0, 1.3);
   TH1D *hdpp = new TH1D("hdpp", "dpp after global cut; dpp;", bins, -range/2, range/2);
 
-  TH1D *hsclose = new TH1D("hsclose", "Distance of CLosest Approach", bins, 0.0, 0.015);
-  TH1D *hzclose = new TH1D("hzclose", "z of Closest Approach", bins, 0.0, 3.0);
-  TH1D *hzclose0 = new TH1D("hzclose0", "z of Closest Approach", bins, 0.0, 3.0);
-  TH1D *hzclose1 = new TH1D("hzclose1", "z of Closest Approach", bins, 0.0, 3.0);
-  TH1D *httheta = new TH1D("httheta", "Polar Scattering Angle", bins, 0.0, 30.0);
-  TH2D *hzvtheta = new TH2D("hzvtheta", "z vs theta ; theta (deg); zclose (m)", bins, 0.0, 30.0, bins, 0.0, 3.0);
+  TH1D *hsclose = new TH1D("hsclose", "Distance of Closest Approach; DOCA (m)", bins, 0.0, 0.015);
+  TH1D *hzclose = new TH1D("hzclose", "z of Closest Approach; z_{close} (m)", bins, 0.0, 3.0);
+  TH1D *hzclose0 = new TH1D("hzclose0", "z of Closest Approach; z_{close} (m)", bins, 0.0, 3.0);
+  TH1D *hzclose1 = new TH1D("hzclose1", "z of Closest Approach; z_{close} (m)", bins, 0.0, 3.0);
+  TH1D *httheta = new TH1D("httheta", "Polar Scattering Angle;#theta_{FPP} (degrees)", bins, 0.0, 30.0);
+  TH1D *htphi = new TH1D("htphi", "Azimuthal Scattering Angle; #phi (rad); Events", 18, -M_PI, M_PI);
+  TH1D *htphipos = new TH1D("htphipos", "Azimuthal Scattering Angle; #phi (rad); Events", 18, -M_PI, M_PI);
+  TH1D *htphineg = new TH1D("htphineg", "Azimuthal Scattering Angle; #phi (rad); Events", 18, -M_PI, M_PI);
+  TH2D *hzvtheta = new TH2D("hzvtheta", "z vs #theta ;#theta (deg);z_{close} (m)", bins, 0.0, 30.0, bins, 0.0, 3.0);
 
+  htphi->Sumw2();  htphipos->Sumw2();  htphineg->Sumw2();
+  
   long nevent=0;
   TTreeFormula *GlobalCut = new TTreeFormula( "GlobalCut", globalcut.GetTitle(), C );
+  TTreeFormula *SelectionCut = new TTreeFormula( "SelectionCut", selectioncut.GetTitle(), C );
 
   int treenum=0, currenttreenum=0;
   int ngoodevs = 0;
+  int ngoodasym = 0;
   //map<int, int> runyield;
   while( C->GetEntry( nevent++ ) && nevent){
     currenttreenum = C->GetTreeNumber();
@@ -262,10 +325,18 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
       }
       GlobalCut = new TTreeFormula( "GlobalCut", globalcut.GetTitle(), C );
       GlobalCut->UpdateFormulaLeaves();
+
+      if (SelectionCut) {
+        delete SelectionCut;
+        SelectionCut = nullptr;
+      }
+      SelectionCut = new TTreeFormula( "SelectionCut", selectioncut.GetTitle(), C );
+      SelectionCut->UpdateFormulaLeaves();
     }
    
     if( nevent % 100000 == 0 ) cout << nevent << endl;
     bool passedcut = GlobalCut->EvalInstance(0) != 0;
+    bool passedselection = SelectionCut->EvalInstance(0) != 0;
 
     if( passedcut ){
       //if( runyield.find(runnum[0]) == runyield.end() ){
@@ -282,172 +353,125 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
       hdpp->Fill( dpp[0]);
       hsclose->Fill( sclose[0]);
       hzclose->Fill( zclose[0]);
-      if (ttheta[0] * 180/ PI() < 1) {
-	hzclose1->Fill(zclose[0]);
+      httheta->Fill( ttheta[0] * 180 / M_PI );
+      hzvtheta->Fill( ttheta[0] * 180 / M_PI, zclose[0]);
+      if (ttheta[0] * 180/ M_PI < 1) {
+	      hzclose1->Fill(zclose[0]);
       } else {
-	hzclose0->Fill(zclose[0]);
+	      hzclose0->Fill(zclose[0]);
+      }
+
+      if (passedselection) {
+        ngoodasym ++;
+        htphi->Fill( tphi[0]);
+        if (true_hel[0] > 0) {
+	        htphipos->Fill( tphi[0]);
+        } else {
+	        htphineg->Fill( tphi[0]);
+        }
       }
     }
     
   }
   
-  // cout << "Print my map:" << endl;
-  //for (const auto& element : runyield){
-  //  cout << element.first << " : " << element.second << "\n";
-  //}
-
-  // Let's do some charge corrections
-  //ifstream fcharge(chargefile);  // read a text file which contains all the total charge values of each run
-  //int runrow;
-  //double chargerow;
-  //map<int, double> chargemap;
-  //while(fcharge >> runrow >> chargerow){
-    //cout << "Read charge: " << runrow << ", " << chargerow << endl;
-    //if( chargemap.find(runrow) == chargemap.end() ){
-  //   chargemap.insert({runrow, chargerow});
-  //}
-  //}
-  //fcharge.close();
-
-  // Let's do some livetime corrections
-  /*long nts=0;
-  double liveT=0;
-  int treenumR=0, currenttreenumR=0;
-  map<int, double> runlivet;
-  map<int, int> runlivetN;
-  int currentRunNumber = -1;
-  while( R->GetEntry( nts++ ) && nts){
-    currenttreenumR = R->GetTreeNumber();
-    if( nts == 1 || currenttreenumR != treenumR ){
-      treenumR = currenttreenumR;
-    }
-
-    TFile* currentfile = R->GetCurrentFile();
-    string fname = currentfile->GetName();
-
-    currentRunNumber = Readfilename(fname);
-
-    liveT = L1A[0] / (TS10[0]+4);
-
-    if( runlivet.find(currentRunNumber) == runlivet.end() ){
-        runlivet.insert({currentRunNumber, liveT});
-	runlivetN.insert({currentRunNumber, 1});
-      }
-      else{
-	runlivet[currentRunNumber] += liveT;
-	runlivetN[currentRunNumber]++;
-      }
-    
-      }*/
-
-  // Our maps of data are:
-  //   runyield (events in each run passing cuts)
-  //   runcharge (charge of each run)
-  //   runlivet (sum of live times in each run)
-  //   runlivetN (number of enteries to get average live time from runlivet map)
-  // map<int, RunData> runmap;  // now merge all this info together and contain it in the RunData struct
-  //for( const auto& [key, valA] : runyield) {
-  //  if (runlivet.count(key) && chargemap.count(key)){
-  //    runmap[key] = { valA, runlivet.at(key) / runlivetN.at(key), chargemap.at(key) };
-  //  }
-  //}
-  /*
-  double normyield = 0;
-  double totalcharge = 0;
-  for( const auto& [run, data] : runmap) {
-    cout << "Run " << run
-	 << ": yield = " << data.events
-	 << ", livetime = " << data.livetime
-	 << ", charge = " << data.charge
-	 << ", normalized yield = " << data.events / data.livetime / data.charge << endl;
-    //normyield += data.events / (data.charge * data.livetime);
-    normyield += data.events; // / data.livetime;
-    totalcharge += data.charge;
-    hYield->Fill( data.events / data.livetime / data.charge);
-  }
-
-  cout << "Elastic Yield with charge and live time normalization = " << normyield/totalcharge << " Events/Coulomb" << "\n" << endl;
-  */
   
   TString outfilepdf = outfilename; // Make a pdf file to save these histograms to
   outfilepdf.ReplaceAll(".root",".pdf");
 
-  // Let's fit curves to our histograms
-  vector<double> bparX, bparY;
-  vector<double> sparX, sparY;
-
-  TF1 *fitBkgrSideX = FitBkgrSide(hdxECAL);  // Fit the background at the sides
-  bparX.push_back(fitBkgrSideX->GetParameter(0));
-  bparX.push_back(fitBkgrSideX->GetParameter(1));
-  bparX.push_back(fitBkgrSideX->GetParameter(2));
-  TF1 *fitAllX = FitGausQuad(hdxECAL, bparX); // Fit signal over the background
-  sparX.push_back(fitAllX->GetParameter(0));
-  sparX.push_back(fitAllX->GetParameter(1));
-  sparX.push_back(fitAllX->GetParameter(2));
-  TF1 *fitSigX = GausOnly(sparX, hdxECAL); // Extract only signal and backgroun
-  TF1 *fitBkgrX = QuadOnly(bparX, hdxECAL);
-
-  
-  TF1 *fitBkgrSideY = FitBkgrSide(hdyECAL);
-  bparY.push_back(fitBkgrSideY->GetParameter(0));
-  bparY.push_back(fitBkgrSideY->GetParameter(1));
-  bparY.push_back(fitBkgrSideY->GetParameter(2));
-  TF1 *fitAllY = FitGausQuad(hdyECAL, bparY);
-  sparY.push_back(fitAllY->GetParameter(0));
-  sparY.push_back(fitAllY->GetParameter(1));
-  sparY.push_back(fitAllY->GetParameter(2));
-  TF1 *fitSigY = GausOnly(sparY, hdyECAL);
-  TF1 *fitBkgrY = QuadOnly(bparY, hdyECAL);
-  
   
   // Make some plots for us to look at
   TCanvas *c1 = new TCanvas("c1","",1600,1200);
+
+  hzclose->SetLineColor(kBlack);
+  hzclose->SetLineWidth(2);
+  hzclose0->SetLineColor(kRed);
+  hzclose0->SetLineWidth(2);
+  hzclose0->SetFillColor(kRed - 9);
+
+  hzclose1->SetLineColor(kBlue);
+  hzclose1->SetLineWidth(2);
+  hzclose1->SetFillColor(kBlue - 9);
+  c1->cd();
   c1->Divide(2,2);
-  c1->cd(1);    hdxECAL->Draw();    fitAllX->Draw("same");    fitBkgrX->Draw("same");    fitSigX->Draw("same");
-  c1->cd(2);    hdyECAL->Draw();    fitAllY->Draw("same");    fitBkgrY->Draw("same");    fitSigY->Draw("same");
-  c1->cd(3);    hdxECAL_v_dyECAL->Draw();
-  c1->cd(4);    hEdivP->Draw();
-  //c1->Update();
+  c1->cd(1);    hsclose->Draw();
+  c1->cd(2);    httheta->Draw();
+  c1->cd(3);    hzclose->Draw();    hzclose0->Draw("same");    hzclose1->Draw("same");
+  // Create legend
+  TLegend *leg = new TLegend(0.6, 0.7, 0.88, 0.88);  // x1,y1,x2,y2 in NDC
+  leg->AddEntry(hzclose,  "All events", "l");
+  leg->AddEntry(hzclose0, "#theta < 1 deg", "fl");  // f = filled
+  leg->AddEntry(hzclose1, "#theta > 1 deg", "fl");
+  leg->SetBorderSize(0);
+  leg->SetFillStyle(0);   // transparent
+  leg->SetTextSize(0.03);
+  leg->Draw();
+  c1->cd(4);    hzvtheta->Draw();
+  c1->Update();
   c1->Print(outfilepdf + "(");  //Open the pdf and make the first page
 
-  //double ElasticYieldX = (fitSigX->Integral(-1,1))/(range/bins);
-  //double ElasticYieldY = (fitSigY->Integral(-1,1))/(range/bins);
 
-  //cout << "Elastic yeild X = " << ElasticYieldX << endl;
-  //cout << "Elastic yeild Y = " << ElasticYieldY << endl;
 
-  // Let's try fitting our dpp distribution
-  vector<double> bparP;
-  vector<double> sparP;
+  // Let's look at the asymmetry now
+  TH1D *htphidif = (TH1D*) htphipos->Clone("htphidif");
+  htphidif->Add(htphineg, -1);
 
-  TF1 *fitBkgrSideP = FitBkgrSide(hdpp);  // Fit the background at the sides
-  bparP.push_back(fitBkgrSideP->GetParameter(0));
-  bparP.push_back(fitBkgrSideP->GetParameter(1));
-  bparP.push_back(fitBkgrSideP->GetParameter(2));
-  TF1 *fitAllP = FitGausQuad(hdpp, bparP); // Fit signal over the background
-  sparP.push_back(fitAllP->GetParameter(0));
-  sparP.push_back(fitAllP->GetParameter(1));
-  sparP.push_back(fitAllP->GetParameter(2));
-  TF1 *fitSigP = GausOnly(sparP, hdpp); // Extract only signal and background
-  TF1 *fitBkgrP = QuadOnly(bparP, hdpp);
+  TH1D *htphiasym = (TH1D*) htphidif->Clone("htphiasym");
+  htphiasym->Divide(htphi); //Our helicity asymmetry
+  htphiasym->SetTitle("Helicity Asymmetry (N_{+}-N_{-})/(N_{+}+N_{-}), fit=s_{1}sin(#phi)+c_{1}cos(#phi)");
+  htphiasym->GetXaxis()->SetTitle("#phi (rad)");
+  htphiasym->GetYaxis()->SetTitle("(N_{+}-N_{-})/(N_{+}+N_{-})");
 
-  double xmin = fitAllP->GetXmin();
-  double xmax = fitAllP->GetXmax();
+  TF1 *fitfunc = new TF1("fitfunc", "[0]*sin(x) + [1]*cos(x)", -M_PI, M_PI);  //Prepare our sin+cos fit for the asymmetry
+  fitfunc->SetParameters(0.1, 0.1);
+  fitfunc->SetParNames("s_{1}", "c_{1}");
+  gStyle->SetOptFit(111);
+  htphiasym->SetStats(kTRUE);
   
-  // double fsig = fitSigP->Integral(xmin, xmax) / fitAllP->Integral(xmin, xmax);
-
-  //cout << "Signal fraction = " << fsig << endl;
-
-  //cout << "Number of sig = " << (fitSigP->Integral(-range/2,range/2))/(range/bins) << endl;
-  //cout << "Number of bkgr = " << (fitBkgrP->Integral(-range/2,range/2))/(range/bins) << endl;
-  //cout << "Number of events = " << (fitAllP->Integral(-range/2,range/2))/(range/bins) << endl;
-
   TCanvas *c2 = new TCanvas("c2","",1600,1200);
+  c2->cd();
   c2->Divide(1,2);
-  c2->cd(1);    hdpp->Draw();    fitAllP->Draw("same");    fitBkgrP->Draw("same");    fitSigP->Draw("same");
-  c2->cd(2);    hsclose->Draw();
-  c2->Print(outfilepdf + "");
+  c2->cd(1);
+  gPad->cd();
+  DrawAsymmetry(htphi,1,kBlack,2,true,true);    // Plotting the helicity separated phi distributions
+  DrawAsymmetry(htphipos,1,kRed,2,false,true);
+  DrawAsymmetry(htphineg,1,kBlue,2,false,true);
+  TLegend *leg2 = new TLegend(0.6, 0.7, 0.88, 0.88);  // x1,y1,x2,y2 in NDC
+  leg2->AddEntry(htphi,  "N_{+}+N_{-}", "p");
+  leg2->AddEntry(htphipos, "N_{+}", "p");
+  leg2->AddEntry(htphineg, "N_{-}", "p");
+  leg2->SetBorderSize(0);
+  leg2->SetFillStyle(0);   // transparent
+  leg2->SetTextSize(0.05);
+  leg2->Draw();
 
+  c2->cd(2);
+  gPad->cd();
+  htphiasym->Fit(fitfunc, "R");   // Fitting the asymmetry
+  DrawAsymmetry(htphiasym, 1, kBlack, 2);
+  int nbins = htphiasym->GetNbinsX(); // Drawing error bars as vertical lines for cleaner visuals
+  for (int i = 1; i <= nbins; ++i) {
+    double x = htphiasym->GetBinCenter(i);
+    double y = htphiasym->GetBinContent(i);
+    double err = htphiasym->GetBinError(i);
+
+    TLine *vline = new TLine(x, y - err, x, y + err);
+    vline->SetLineColor(kBlack);
+    vline->SetLineWidth(1);
+    vline->Draw("SAME");
+  }
+  c2->Update();
+  TPaveStats *st = (TPaveStats*)htphiasym->GetListOfFunctions()->FindObject("stats");   //Adjusting stat box for cleaner visuals
+  if (st) {
+    st->SetX1NDC(0.75); // left
+    st->SetX2NDC(0.95); // right
+    st->SetY1NDC(0.75); // bottom
+    st->SetY2NDC(0.9); // top
+    st->Draw();
+  }
+  c2->Modified();
+  c2->Update();
+  c2->Print(outfilepdf + "");
+  
 
   /**** Summary Canvas ****/
   TCanvas *cSummary = new TCanvas("cSummary","Summary");
@@ -459,9 +483,13 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
   t1->SetTextColor(kBlue);  
   pt->AddText(Form(" Total # events analyzed: %lld ",nevent));
   pt->AddText(Form("Total # events passed global cuts: %lld", ngoodevs));
+  pt->AddText(Form("Total # events passed selection cuts: %lld", ngoodasym));
   TText *t2 = pt->AddText(" Global cuts: ");
   t2->SetTextColor(kRed);
   AddWrappedText(pt, globalcut.GetTitle());
+  TText *t3 = pt->AddText(" Selection cuts: ");
+  t3->SetTextColor(kRed);
+  AddWrappedText(pt, selectioncut.GetTitle());
   //  TText *t3 = pt->AddText(Form(" Average Charge Normalized Elastic Yield: %.0f", normyield * fsig / totalcharge));
   //t3->SetTextColor(kRed);
   //pt->AddText(Form(" Signal fraction: %.5f ",fsig));
@@ -469,6 +497,7 @@ void GetAsymmetry( const char *configfilename, const char *chargefile="runCharge
   //t4->SetTextColor(kRed);
   pt->Draw();
   cSummary->Print(outfilepdf + ")"); // Close out the pdf
+
   
   
   //  cout << "Total number of events: " << nevent << endl;
